3c519638dc325cbe9abc71337d96e6051ba48d04
tanqiyuan <tanqiyuaneric@163.com>
2023-06-09 09:12:27 +0800

    models created; migration made; edited urls and views for mainsite

--- mainsite/models.py
+++ mainsite/models.py
@@ -5,4 +5,6 @@ from django.db import models
 class History(models.Model):
     wechat_id = models.CharField(max_length=100, name='微信号')
     name = models.CharField(max_length=40, name="姓名")
-    number = models.IntegerField()
+    number = models.IntegerField(name="数字")
+    grade = models.PositiveSmallIntegerField(name='年级')
+    class_number = models.PositiveSmallIntegerField(name='班级')
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,6 +1,35 @@
+from django.contrib.auth.decorators import login_required
+from django.forms import ModelForm
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from mainsite.models import History
 
+
+class Form(ModelForm):
+    class Meta:
+        model = History
+        fields = ['微信号']
+
+
+@login_required
 def index(request):
-    return HttpResponse("Hello, world.")
+    return HttpResponse('hello world')
+
+
+def register(request):
+    if request.method == 'GET':
+        form = Form()
+        return render(request, 'register.html', {'forms': form})
+
+    form = Form(data=request.POST)
+    if form.is_valid():
+        user_input_code = form.cleaned_data.pop('code')
+        code = request.session.get('code', '')
+
+        if code.upper() != user_input_code.upper():
+            form.add_error("code", "验证码错误")
+            return render(request, 'register.html', {'forms': form})
+
+    else:
+        return render(request, 'register.html', {'forms': form})
