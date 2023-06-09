0798975b1cd35724ab27db44bc19fdaac072ed78
tanqiyuan <tanqiyuaneric@163.com>
2023-06-09 08:36:40 +0800

    models created; migration made; edited urls and views for mainsite

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class History(models.Model):
+    wechat_id = models.CharField(max_length=100, name='微信号')
+    name = models.CharField(max_length=40, name="姓名")
+    number = models.IntegerField()
---
+++ mainsite/urls.py
@@ -0,0 +1,13 @@
+# -*- coding = utf-8 -*-
+# @Time : 2023/6/9 8:32
+# @Author : tqy
+# @File : urls.py.py
+# @Software : PyCharm
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
