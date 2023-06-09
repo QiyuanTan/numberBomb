7e98669b6f072cd41a8e0c0a10cef60748b3a81f
tanqiyuan <tanqiyuaneric@163.com>
2023-06-09 11:05:18 +0800

    update files

--- mainsite/views.py
+++ mainsite/views.py
@@ -2,16 +2,25 @@ from django.contrib.auth.decorators import login_required
 from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
 from mainsite.models import History
+import random
+import string
+from django.db import IntegrityError
+
+
+def generate_random_string(length):
+    """生成指定长度的随机字符串"""
+    letters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(letters) for _ in range(length))
 
 
 class RegisterForm(forms.Form):
     wechat_id = forms.CharField(label='微信号')
     name = forms.CharField(label='姓名')
-    grade = forms.CharField(label='年级')
-    class_number = forms.CharField(label='班级')
-    number = forms.CharField(label='数字')
+    grade = forms.IntegerField(label='年级')
+    class_number = forms.IntegerField(label='班级')
+    number = forms.IntegerField(label='数字')
 
 
 @login_required
@@ -22,16 +31,24 @@ def index(request):
 def register(request):
     if request.method == 'GET':
         form = RegisterForm
-        return render(request, '../user/templates/templates/register.html', {'forms': form})
+        return render(request, 'register.html', {'forms': form})
 
     form = RegisterForm(data=request.POST)
     if form.is_valid():
-        user_input_code = form.cleaned_data.pop('code')
-        code = request.session.get('code', '')
+        name = form.cleaned_data.get('name')
+        wechat_id = form.cleaned_data.get('wechat_id')
+        grade = form.cleaned_data.get('grade', '')
+        class_number = form.cleaned_data.get('class_number')
+        number = form.cleaned_data.get('number')
+        print(wechat_id)
+        try:
+            user = User.objects.create_user(username=name, password=generate_random_string(10))
+            user.save()
+        except IntegrityError:
+            form.add_error("name", "请勿重复提交")
+            return render(request, 'register.html', {'forms': form})
 
-        if code.upper() != user_input_code.upper():
-            form.add_error("code", "验证码错误")
-            return render(request, '../user/templates/templates/register.html', {'forms': form})
-
+        history = History(user=user, wechat_id=wechat_id)
+        history.save()
     else:
-        return render(request, '../user/templates/templates/register.html', {'forms': form})
+        return render(request, 'register.html', {'forms': form})
--- numberboom/settings.py
+++ numberboom/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = "django-insecure-h&&gfra648l!^j*o=bw91x))5-jr8t+_bq3m5k2tsoo&7tmd$r
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "numberboom.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -113,7 +108,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -123,3 +117,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = '/register'
